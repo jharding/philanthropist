db543aa576027565e0530c6739820218571ff700
Jake Harding <jachardi@cisco.com>
2012-04-01 16:46:28 -0700

    using grunt

---
+++ grunt.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        lint: {
+            test: ['test/*.js'],
+            lib: ['lib/options/*.js', 'lib/background/*.js']
+        },
+        qunit: {
+            index: ['test/test.html']
+        },
+        jshint: {
+            // enforcing options
+            immed: false,
+            strict: false,
+            trailing: false,
+            // environments
+            browser: true,
+            jquery: true,
+            white: false
+        }
+    });
+};
