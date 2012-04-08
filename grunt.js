fa049a4b65ca0377583ef7dd2c31ae4ae85a0982
Jake Harding <jacob.s.harding@gmail.com>
2012-04-08 01:59:48 -0700

    adding some grunt tasks for production.

--- grunt.js
+++ grunt.js
@@ -1,16 +1,25 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         less: {
-            all: {
+            dev: {
                 src: ['public/css/style.less'],
                 dest: 'public/css/style.css'
+            },
+            prod: {
+                src: ['public/css/style.less'],
+                dest: 'public/css/style.css',
+                options: {
+                    yuicompress: true
+                }
             }
         },
         watch: {
             files: 'public/**/*.less',
-            tasks: 'less'
+            tasks: 'less:dev'
         }
     });
 
     grunt.loadNpmTasks('grunt-less');
+
+    grunt.registerTask('prod', 'less:prod');
 };
